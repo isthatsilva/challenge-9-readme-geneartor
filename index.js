de79ab99b0d367efd11b88b900445b55fea47ccf
isthatsilva <111096274+isthatsilva@users.noreply.github.com>
2022-12-19 17:14:37 -0500

    updated error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateReadme');
+const generateReadme = require('./utils/generateReadme');
 
 const questions = [
     {
